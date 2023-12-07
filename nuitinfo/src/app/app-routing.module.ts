4be3c9144d4d7081b4ab0ae688aaf5a9ce2db070
pluypaert <pierre.luypaert@insa-rouen.fr>
2023-12-07 19:45:02 +0100

    refactor main structure

--- nuitinfo/src/app/app-routing.module.ts
+++ nuitinfo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TetrisComponent } from './components/tetris/tetris.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tetris', component: TetrisComponent },
+  { path: '', redirectTo: '/tetris', pathMatch: 'full' }, // Redirect to 'history' by default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
