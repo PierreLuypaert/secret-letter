e66979cfcd38ea4a27cccded81ba8c8b83a4d526
pluypaert <pierre.luypaert@insa-rouen.fr>
2023-12-08 05:04:52 +0100

    menu

--- nuitinfo/src/app/app-routing.module.ts
+++ nuitinfo/src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { PremiereScenePereNoelComponent } from './components/premiere-scene-pere
 import { CarteComponent } from './components/carte/carte.component';
 import { ArctiqueComponent } from './components/arctique/arctique.component';
 import { OceanieComponent } from './components/oceanie/oceanie.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
   { path: 'oceanie', component: OceanieComponent, data: { animation: 'fadeInOut' } },
   { path: 'arctique', component: ArctiqueComponent, data: { animation: 'fadeInOut' } },
   { path: 'carte', component: CarteComponent, data: { animation: 'fadeInOut' } },
+  { path: 'menu', component: MenuComponent, data: { animation: 'fadeInOut' } },
   { path: 'home', component: PremiereScenePereNoelComponent, data: { animation: 'fadeInOut' } },
   { path: 'tetris', component: TetrisComponent, data: { animation: 'fadeInOut' } },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
 ];
 
 @NgModule({
